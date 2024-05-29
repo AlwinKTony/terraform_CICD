3f9d9812b3b65843cba9e0396a3b9784acfb8ace
AlwinKTony <134669373+AlwinKTony@users.noreply.github.com>
2024-05-29 03:32:32 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0ca2e925753ca2fb4"
+    instance_type = "t2.medium"
+    tags = {
+      Name = "dev-ec2"
+    }
+}
